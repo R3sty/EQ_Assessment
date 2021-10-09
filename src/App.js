08c83c2063bedcb20683a30370f3e7bdc84f7940
Resty <54195157+R3sty@users.noreply.github.com>
2021-10-09 21:08:38 +0900

    added React components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
-import "./App.css";
+import React, { useState } from "react";
+import { AssessmentContext } from "./Helpers/Context";
+
+//components
+import HomeScreen from "./components/HomeScreen";
+import QuestionCard from "./components/QuestionCard";
+import EndScreen from "./components/EndScreen";
+//material-ui
+import Grid from "@material-ui/core/Grid";
+import { Box } from "@material-ui/core";
 
 function App() {
+  const [view, setView] = useState("home");
+  const [score, setScore] = useState(0);
+
   return (
-    <div className="App">
-      <h1>EQTest deployment</h1>
-    </div>
+    <React.Fragment>
+      <Grid
+        container
+        spacing={0}
+        justifyContent="center"
+        alignItems="center"
+        style={{ height: "100vh" }}
+      >
+        <Grid item xs="auto">
+          <AssessmentContext.Provider
+            value={{ view, setView, score, setScore }}
+          >
+            <Box textAlign="center">
+              <h1>EQ Assessment</h1>
+              {view === "home" && <HomeScreen />}
+              {view === "start assessment" && <QuestionCard />}
+              {view === "end" && <EndScreen />}
+            </Box>
+          </AssessmentContext.Provider>
+        </Grid>
+      </Grid>
+    </React.Fragment>
   );
 }
 
