76a788cbdefd49e03d76d7efb680eb521f905640
Resty <54195157+R3sty@users.noreply.github.com>
2022-01-11 09:38:04 +0900

    moved setAPI key inside handler

--- functions/sendEmail.js
+++ functions/sendEmail.js
@@ -1,8 +1,8 @@
 const parsedData = JSON.parse(event.body);
 const sgMail = require("@sendgrid/mail");
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 exports.handler = async function (event) {
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
   const msg = {
     to: parsedData.email, // Change to your recipient
     from: process.env.SENDER_EMAIL, // Change to your verified sender
